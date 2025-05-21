8dd8fa49e930e4e0292ff8e2eb45b3a048bcbfb2
matsubara457 <tatsuhiro.matsubara@nahato.co.jp>
2025-05-21 22:12:00 +0900

    タスクの追加機能・削除機能

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ const onClickAdd = () => {
   // textboxの値を取得し初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
 
   // li生成
   const li = document.createElement("li");
@@ -18,8 +17,28 @@ const onClickAdd = () => {
   p.className = "todo-item";
   p.innerText = inputText;
 
+// button(完了)タグ生成
+const completeButton = document.createElement("button");
+completeButton.innerText = "完了";
+completeButton.addEventListener("click",() => {
+alert("完了")
+});
+
+// button(削除)タグ生成
+const deleteButton = document.createElement("button");
+deleteButton.innerText = "削除";
+deleteButton.addEventListener("click",() => {
+  // 押された削除ボタンの親にあるliタグを未完了リストから削除
+  const deleteTarget = deleteButton.closest("li");
+  document.getElementById('incomplete-list').removeChild(deleteTarget);
+  
+});
+
+
   // liタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了リストに追加
